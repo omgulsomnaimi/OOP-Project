f674608ddf1b7109880dd3741128db981338f1c9
omgulsomnaimi <omgulsomnaimi@gmail.com>
2025-10-28 22:11:40 +0430

    Update main_work.py

--- main_work.py
+++ main_work.py
@@ -8,14 +8,12 @@ for question in question_data:
     question_text = question["text"]
     question_answer = question["answer"]
     new_question = Question(question_text, question_answer)
-    # We save the object of new question in question bank so we append it
     question_bank.append(new_question)
 
+quiz = QuizBrain(question_bank)
 
-quiz = QuizBrain(question_bank) # حالا لیست سوالات question_bank در فایل quiz_brain داخل attribute question_list ذخیره میشود
-
-while quiz.still_has_question(): #if quiz still has questions remaining:
+while quiz.still_has_question():
     quiz.next_question()
 
-print("You have completed the quiz")
+print("You have completed the quiz!")
 print(f"Your final score is: {quiz.score}/{quiz.question_number}")
