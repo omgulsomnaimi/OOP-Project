b1cf5d133612b7f51864d591286e5b0f83f7c6a4
omgulsomnaimi <omgulsomnaimi@gmail.com>
2025-10-28 22:10:54 +0430

    Update quiz_brain.py

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,16 +1,11 @@
 class QuizBrain:
-
     def __init__(self, q_list):
-        self.question_number = 0   # Attribute of the class
-        self. question_list = q_list # Attribute of the class # قیمت این attribute را نیاز است که از parameter فنکشن init دریافت کنیم
+        self.question_number = 0
+        self.question_list = q_list
         self.score = 0
 
     def still_has_question(self):
-        if self.question_number < len (self.question_list):
-            return True
-        else:
-            return False
-
+        return self.question_number < len(self.question_list)
 
     def next_question(self):
         current_question = self.question_list[self.question_number]
@@ -22,7 +17,6 @@ class QuizBrain:
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
             print("You got it right!")
-
         else:
             print("That's wrong!")
             print(f"The correct answer was: {correct_answer}")
